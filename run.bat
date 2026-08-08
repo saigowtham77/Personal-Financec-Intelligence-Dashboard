19f0168e40767b692b5a75cdc654e139d306aa45
saigowtham77 <106604754+saigowtham77@users.noreply.github.com>
2026-08-08 17:52:28 +0530

    Upload run.bat

---
+++ run.bat
@@ -0,0 +1,8 @@
+@echo off
+title Python Finance Dashboard
+echo ==================================================
+echo   Starting Python Finance Dashboard Application
+echo ==================================================
+cd /d "%~dp0"
+python app.py
+pause
